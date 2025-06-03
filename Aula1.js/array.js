9a01be8a8a6dea4e34707cf64917b33765f51b17
Eduardo <dudulazzari08@gmail.com>
2025-06-03 15:47:05 -0300

    Aula 1

---
+++ Aula1.js/array.js
@@ -0,0 +1,27 @@
+//estudando arrays e suas funções em js; EM JAVASCRIPT o array é basicamente uma lista, mas em outras linguagens são coisas diferentes.
+
+let frutas = ["Maçã", "Banana", "Laranja", "Goiaba"];
+
+frutas.sort()
+console.log(frutas[0])
+console.log(frutas[1])
+
+//Adiciona um elemento no final
+frutas.push("Uva");
+
+//Adiciona um elemento no início
+frutas.unshift("Melancia");
+
+//remove o último elemento
+frutas.pop();
+
+//Remove o primeiro elemento
+frutas.shift();
+
+console.log(frutas);
+
+let roupas = ["Camisa", "Calça", "Shorts", "Meia", "Blusa"]
+
+roupas.push("Touca")
+console.log(roupas)
+
